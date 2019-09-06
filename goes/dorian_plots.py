919f7a44940f4b6f188f98d6dc3437df6a0dbae4
mnichol3 <nicholson.matt94@gmail.com>
2019-09-06 14:08:31 -0400

    Dev tweaks

--- goes/dorian_plots.py
+++ goes/dorian_plots.py
@@ -1,11 +1,20 @@
-from utils import read_file_abi
+from utils import read_file_abi, get_fnames_from_dir, plot_mercator
 
 
-vis_path = '/media/mnichol3/tsb1/data/abi/dorian/OR_ABI-L1b-RadM1-M6C02_G16_s20192481730202_e20192481730260_c20192481730298.nc'
-inf_path = '/media/mnichol3/tsb1/data/abi/dorian/OR_ABI-L2-CMIPM1-M6C13_G16_s20192481730202_e20192481730271_c20192481730342.nc'
+# inf_base = '/media/mnichol3/tsb1/data/abi/dorian/inf'
+# fnames = get_fnames_from_dir(inf_base)
 
-vis = read_file_abi(vis_path)
-inf = read_file_abi(inf_path)
+inf_file = '/media/mnichol3/tsb1/data/abi/dorian/inf/OR_ABI-L2-CMIPM1-M6C13_G16_s20192491031171_e20192491031240_c20192491031307.nc'
 
-for key, val in vis.items():
-    print(key, val)
+plot_comms = {'save': False,
+              'show': True,
+              'outpath': None}
+
+inf_data = read_file_abi(inf_file)
+print(inf_data['sector'])
+# plot_mercator(inf_data, plot_comms)
+
+# for key, val in inf_data.items():
+#     print(key)
+#
+# print(inf_data['data_units'])
