53e9fc8bd099e4da524b1ca2a698159b6a0f01bb
mnichol3 <nicholson.matt94@gmail.com>
2019-09-06 16:31:14 -0400

    dev tweaks

--- goes/dorian_plots.py
+++ goes/dorian_plots.py
@@ -7,16 +7,16 @@ from utils import read_file_abi, get_fnames_from_dir, plot_mercator
 inf_file = '/media/mnichol3/tsb1/data/abi/dorian/inf/OR_ABI-L2-CMIPM1-M6C13_G16_s20192491031171_e20192491031240_c20192491031307.nc'
 outpath = '/home/mnichol3/Pictures/wx/2019-Dorian/plots'
 
-# plot_comms = {'save': False,
-#               'show': True,
-#               'outpath': None}
+plot_comms = {'save': False,
+              'show': True,
+              'outpath': None}
 
-plot_comms = {'save': True,
-              'show': False,
-              'outpath': outpath}
+# plot_comms = {'save': True,
+#               'show': False,
+#               'outpath': outpath}
 
 inf_data = read_file_abi(inf_file)
-plot_mercator(inf_data, plot_comms)
+# plot_mercator(inf_data, plot_comms)
 
 # for key, val in inf_data.items():
 #     print(key)
