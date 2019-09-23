bb68af99bf6f17f61f836fb1383706c7a55509ab
mnichol3 <nicholson.matt94@gmail.com>
2019-09-23 12:38:56 -0400

    Add AWIPS & AWS functionality to read_file_glm func
    * read_file_glm func now calls sub-functions to read the GLM file
      after determining the file type
    * Add _read_file_glm_fed func
      - Reads AWIPS/WMO-compliant GLM Flash Extent Density file, returns
        a LocalGLMFile object
    * Add __read_file_glm_egf func
      - Reads a GLM file downloaded from AWS which contains Event, Group,
        and Flash data and returns a LocalGLMFile object

--- goes/utils.py
+++ goes/utils.py
@@ -252,9 +252,46 @@ def fname_gen(base_path):
 
 
 
-def read_file_glm(abs_path, window=False):
+def read_file_glm(abs_path, product='f', window=False):
     """
     Read a GLM file into a glm_obj
+
+    Parameters
+    ----------
+    abs_path : str
+        Absolute path to the GLM file to open & read
+    product : str
+        Product (out of events, groups, and flashes) to read. Only used for
+        _read_file_glm_egf func. Default is 'f'
+        Valid strings: 'f': Flashes, 'g': Groups, 'e': Events, 'a': All (events, groups, & flashes)
+    window : bool, optional
+        If True, the 5-min GLM window data is used. If False, 1-min data is used
+        Default: False
+
+    Returns
+    -------
+    glm_obj : LocalGLMFile object
+        LocalGLMFile object containing the data from the glm file
+
+    Dependencies
+    ------------
+    > localglmfile.py
+    > netCDF4.Dataset
+    """
+    data_dict = {}
+    file_type_re = r'(\w{4}\d{2}_KNES)'
+
+    if (re.match(abs_path, file_type_re)):
+        _read_file_glm_fed(abs_path, window=window)
+    else:
+        _read_file_glm_egf(abs_path, product=product)
+
+
+
+def _read_file_glm_fed(abs_path,  window=False):
+    """
+    Read an AWIPS-compliant GLM Flash Extent Density file into a glm_obj.
+    Called by read_file_glm func
 
     Parameters
     ----------
@@ -274,7 +311,6 @@ def read_file_glm(abs_path, window=False):
     > localglmfile.py
     > netCDF4.Dataset
     """
-    data_dict = {}
     f_path = trim_header(abs_path)
 
     fh = Dataset(f_path, 'r')
@@ -299,7 +335,85 @@ def read_file_glm(abs_path, window=False):
     fh.close()
     fh = None
 
-    glm_obj = LocalGLMFile(f_path)
+    data_dict['data_type'] = 'Flash Extent Density'
+
+    glm_obj = LocalGLMFile(f_path, 'awips')
+    glm_obj.set_data(data_dict)
+
+    return glm_obj
+
+
+
+def _read_file_glm_egf(abs_path, product='f'):
+    """
+    Read a GLM file into a glm_obj
+
+    Parameters
+    ----------
+    abs_path : str
+        Absolute path to the GLM file to open & read
+    product : str
+        Product (out of events, groups, and flashes) to read. Default is 'f'
+        Valid strings: 'f': Flashes, 'g': Groups, 'e': Events
+
+    Returns
+    -------
+    glm_obj : LocalGLMFile object
+        LocalGLMFile object containing the data from the glm file
+
+    Dependencies
+    ------------
+    > localglmfile.py
+    > netCDF4.Dataset
+    """
+    if (not product in ['e', 'f', 'g']):
+        raise ValueError("Invalid product argument. Valid: 'e', 'f', 'g'")
+
+    fh = Dataset(abs_path, 'r')
+
+    data_dict['long_name'] = fh.variables['goes_lat_lon_projection'].long_name
+    data_dict['lat_0'] = fh.variables['nominal_satellite_subpoint_lat'][:]
+    # File states lon_0 is -75.2, AWIPS states its -75.0
+    # data_dict['lon_0'] = fh.variables['nominal_satellite_subpoint_lon'][:]
+    data_dict['lon_0'] = -75.2
+    data_dict['semi_major_axis'] = fh.variables['goes_lat_lon_projection'].semi_major_axis
+    data_dict['semi_minor_axis'] = fh.variables['goes_lat_lon_projection'].semi_minor_axis
+    data_dict['height'] = fh.variables['nominal_satellite_height'][:]
+    data_dict['inv_flattening'] = fh.variables['goes_lat_lon_projection'].inverse_flattening
+    data_dict['sweep_ang_axis'] = 'x'
+
+    if (product == 'f'):
+        data_dict['x'] = fh.variables['flash_lon'][:]
+        data_dict['y'] = fh.variables['flash_lat'][:]
+        flash_stats = {'area': fh.variables['flash_area'][:],
+                       'energy': fh.variables['flash_energy'][:],
+                       'count': fh.variables['flash_count'][:],
+                       'id': fh.variables['flash_id'][:]}
+        data_dict['data'] = flash_stats
+        data_dict['data_type'] = 'Flashes'
+    elif (product == 'e'):
+        data_dict['x'] = fh.variables['event_lon'][:]
+        data_dict['y'] = fh.variables['event_lat'][:]
+        event_stats = {'area': None,
+                       'energy': fh.variables['event_energy'][:],
+                       'count': fh.variables['event_count'][:],
+                       'id': fh.variables['event_id'][:]}
+        data_dict['data'] = event_stats
+        data_dict['data_type'] = 'Events'
+    elif (product == 'g'):
+        data_dict['x'] = fh.variables['group_lon'][:]
+        data_dict['y'] = fh.variables['group_lat'][:]
+        group_stats = {'area': fh.variables['group_area'][:],
+                       'energy': fh.variables['group_energy'][:],
+                       'count': fh.variables['group_count'][:],
+                       'id': fh.variables['group_id'][:]}
+        data_dict['data'] = group_stats
+        data_dict['data_type'] = 'Groups'
+
+    fh.close()
+    fh = None
+
+    glm_obj = LocalGLMFile(f_path, 'aws')
     glm_obj.set_data(data_dict)
 
     return glm_obj
