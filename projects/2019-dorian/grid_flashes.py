2fdc6c73465191d04ade604d263c2d24a2c83bc3
mnichol3 <nicholson.matt94@gmail.com>
2019-09-30 18:25:30 -0400

    Add multithreading

--- projects/2019-dorian/grid_flashes.py
+++ projects/2019-dorian/grid_flashes.py
@@ -3,9 +3,71 @@ import re
 import glob
 import subprocess
 from datetime import datetime, timedelta
+import logging
+import concurrent.futures
 
 import glmtools
 from glmtools.io.glm import GLMDataset
+
+import warnings
+
+
+
+def calc_date_chunks(start, end):
+    """
+    Creates 6-hr time chunks
+
+    Parameters
+    ----------
+    start : str
+        Format: 'MM-DD-YYYY-HH:MM'
+    end : str
+        Format: 'MM-DD-YYYY-HH:MM'
+
+    Returns
+    --------
+    chunks : list of tuples of (str, str)
+        stf format: 'MM-DD-YYYY-HH:MM'
+
+    Dependencies
+    -------------
+    > datetime
+    """
+    chunks = []
+
+    start = datetime.strptime(start, '%m-%d-%Y-%H:%M')
+    end = datetime.strptime(end, '%m-%d-%Y-%H:%M')
+    prev = start
+
+    while (prev <= end):
+        # Increment 6 hours
+        curr = prev + timedelta(seconds=21600)
+
+        chunks.append((prev, curr))
+        prev = curr
+
+    """
+    Adjust the last tuple incase the time period doesnt divide evenly into
+    6-hr periods
+
+    Ex
+    ---
+    Unadjusted:
+        in:  calc_date_chunks('09-01-2019-15:00', '09-06-2019-13:00')[-1]
+        out: ('09-06-2019-09:00', '09-06-2019-15:00')
+
+    Adjusted:
+        in:  calc_date_chunks('09-01-2019-15:00', '09-06-2019-13:00')[-1]
+        out: ('09-06-2019-09:00', '09-06-2019-13:00')
+
+    """
+    if (chunks[-1][1] > end):
+        chunks = chunks[:-1]
+        prev = prev - timedelta(seconds=21600)
+        # chunks.append((prev.strftime('%m-%d-%Y-%H:%M'), end.strftime('%m-%d-%Y-%H:%M')))
+        chunks.append((prev, end))
+
+    return chunks
 
 
 
@@ -20,12 +82,16 @@ def get_files_for_date_time(base_path, date_time):
     date_time : str
         Date and time of the desired GLM files.
         Format: YYYY-MM-DD HH:MM:SS
+                        or
+                YYYY-MM-DD HH:MM
     """
     fnames = []
     scantime_re = re.compile(r'_s(\d{11})')
 
-    # date_time = datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')
-    date_time = datetime.strptime(date_time[:-3], '%Y-%m-%d %H:%M')
+    try:
+        date_time = datetime.strptime(date_time, '%Y-%m-%d %H:%M')
+    except:
+        date_time = datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')
 
     # Parse the subdirectory path for the desired date & time of the GLM file
     subdir_path = os.path.join(base_path, str(date_time.timetuple().tm_yday), str(date_time.hour).zfill(2))
@@ -60,40 +126,59 @@ def grid_flash_chunk(start_date, end_date):
     make_grid_path = '/home/mnichol3/Coding/glmtools/glmtools/examples/grid/make_GLM_grids.py'
 
     curr_dt = start_date
+    next_dt = start_date + timedelta(seconds=60)
 
     while (curr_dt <= end_date):
         curr_dt_str = curr_dt.strftime('%Y-%m-%d %H:%M:%S')
-        print('Processing: {}'.format(curr_dt_str[:-3]))
+        print('\nProcessing: {} to {}'.format(curr_dt_str[:-3], next_dt.strftime('%Y-%m-%d %H:%M')))
 
         subdir_path = os.path.join(local_glm_path, curr_dt.strftime('%j/%H'))
 
         # Get list of absolute paths of GLM files for the current date & time
-        curr_fames = get_files_for_date_time(local_glm_path, curr_dt_str)
+        curr_fnames = get_files_for_date_time(local_glm_path, curr_dt_str)
 
         cmd = "python {0} -o {1}"
         cmd += " --fixed_grid --split_events --goes_position=east --goes_sector=conus"
         cmd += " --dx=2.0 --dy=2.0"
         cmd += " --start={3} --end={4} {2}"
 
-        cmd = cmd.format(make_grid_path, outpath, ' '.join(curr_fames),
-                        curr_dt.isoformat(), end_date.isoformat())
+        cmd = cmd.format(make_grid_path, outpath, ' '.join(curr_fnames),
+                        curr_dt.isoformat(), next_dt.isoformat())
 
         out_bytes = subprocess.check_output(cmd.split())
 
         grid_dir_base = outpath
         nc_files = glob.glob(os.path.join(grid_dir_base, curr_dt.strftime('%j/%H'),'*.nc'))
-        for f in nc_files:
-            print(f)
+        # for f in nc_files:
+        #     print(f)
 
-        curr_dt = curr_dt + timedelta(seconds=60)
+        curr_dt = next_dt
+        next_dt = next_dt + timedelta(seconds=60)
 
 
 
 def main():
+    log_format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=log_format, level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
+
+    num_threads = 3
+
     start_date = datetime(2019, 8, 24, 12, 0)  # 24 Aug 2019 12:00z
     end_date = datetime(2019, 8, 25, 12, 0)
     # end_date = datetime(2019, 9, 9, 0, 0)      # 09 Sep 2019 00:00z
-    grid_flash_chunk(start_date, end_date)
 
+    time_chunks = calc_date_chunks(start_date.strftime('%m-%d-%Y-%H:%M'), end_date.strftime('%m-%d-%Y-%H:%M'))
+    # for chunk in time_chunks:
+    #     # grid_flash_chunk(chunk[0], chunk[1])
+    #     print(chunk)
+
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+        future_thread = {executor.submit(grid_flash_chunk, chunk[0], chunk[1]):
+                                         chunk for chunk in time_chunks}
+
+        for future in concurrent.futures.as_completed(future_thread):
+            result = future.result()
+            print("Finished processing!")
 
 main()
