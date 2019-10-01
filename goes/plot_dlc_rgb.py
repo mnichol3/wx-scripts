0e48cd8c20a8ff7d09accb1d8fb8bed4efd7657c
mnichol3 <nicholson.matt94@gmail.com>
2019-10-01 13:50:36 -0400

    Add ffmpeg cmds

--- goes/plot_dlc_rgb.py
+++ goes/plot_dlc_rgb.py
@@ -12,18 +12,13 @@ def pp_keys(dict):
         print('{} {}  {}'.format(key, '-->'.rjust(len(max_len_key) - len(key) + 3, '-'), type(val)))
 
 
-# base_path = '/media/mnichol3/tsb1/data/abi'
-# f_mcmip = 'OR_ABI-L2-MCMIPC-M6_G16_s20192591626148_e20192591628521_c20192591629067.nc'
-
-# base_path = '/media/mnichol3/tsb1/data/abi/20190918'
-# f_mcmip = 'OR_ABI-L2-MCMIPC-M6_G16_s20192611821160_e20192611823533_c20192611824108.nc'
-
-# plot_comms = {'save': False,
-#               'show': True,
-#               'outpath': None}
-
 
 def main():
+    """
+    ffmpeg cmd:
+        mp4:  ffmpeg -i image-%03d.png -c:v libx264 -pix_fmt yuv420p out.mp4
+        webm: ffmpeg -i image-%03d.png video.webm
+    """
     base_path = '/media/mnichol3/pmeyers1/MattNicholson/abi/273'
     out_path = '/media/mnichol3/pmeyers1/MattNicholson/goes_rgb/day_land_cloud/2019-09-30'
 
